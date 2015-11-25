c157df78f57627096648335119d9d872a7f27c7a
princessGasmask <nile.horton@gmail.com>
2015-11-25 15:47:04 -0600

    Initial Commit
    Test.

---
+++ GasmaskShyvana/Modes/Harrass.cs
@@ -0,0 +1,48 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+
+// Using the config like this makes your life easier, trust me
+using Settings = GasmaskShyvana.Config.Modes.Harass;
+
+namespace GasmaskShyvana.Modes
+{
+    public sealed class Harass : ModeBase
+    {
+        public override bool ShouldBeExecuted()
+        {
+            // Only execute this mode when the orbwalker is on harass mode
+            return Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass);
+        }
+
+        public override void Execute()
+        {
+            // TODO: Add harass logic here
+            // See how I used the Settings.UseQ and Settings.Mana here, this is why I love
+            // my way of using the menu in the Config class!
+            if (Settings.UseE && E.IsReady())
+            {
+                var target = TargetSelector.GetTarget(E.Range, DamageType.Physical);
+                if (E.IsReady() && E.IsInRange(target) && target != null)
+                {
+                    E.Cast(target);
+                }
+            }
+            if (Settings.UseW && W.IsReady())
+            {
+                var target = TargetSelector.GetTarget(E.Range, DamageType.Physical);
+                if (W.IsReady() && target != null)
+                {
+                    W.Cast();
+                }
+            }
+            if (Settings.UseQ && Q.IsReady())
+            {
+                var target = TargetSelector.GetTarget(Q.Range, DamageType.Physical);
+                if (Q.IsReady() && Q.IsInRange(target) && target != null)
+                {
+                    Q.Cast();
+                }
+            }
+        }
+    }
+}
---
+++ GasmaskShyvana/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using System.Net;
+
+namespace GasmaskShyvana
+{
+    public static class Program
+    {
+        // Change this line to the champion you want to make the addon for,
+        // watch out for the case being correct!
+        public const string ChampName = "Shyvana";
+
+        public static void Main(string[] args)
+        {
+            // Wait till the loading screen has passed
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            // Verify the champion we made this addon for
+            if (Player.Instance.ChampionName != ChampName)
+            {
+                // Champion is not the one we made this addon for,
+                // therefore we return
+                return;
+            }
+
+            //Version Check
+            string currentVersion = "0.1";
+            Chat.Print("GasmaskShyvana Version: " + currentVersion + " - LOADED.");
+            Chat.Print("Checking online version..");
+            if (new WebClient().DownloadString("https://raw.githubusercontent.com/incaner/CancerBuddy/master/CancerDarius/Version.txt") != currentVersion)
+                Chat.Print("Old CancerDarius version!");
+            else
+                Chat.Print("You have the last version of CancerDarius addon.");
+
+            // Initialize the classes that we need
+            Config.Initialize();
+            SpellManager.Initialize();
+            ModeManager.Initialize();
+
+            // Listen to events we need
+            Drawing.OnDraw += OnDraw;
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            // Draw range circles of our spells
+            Circle.Draw(Color.Red, SpellManager.Q.Range, Player.Instance.Position);
+            // TODO: Uncomment if you want those enabled aswell, but remember to enable them
+            // TODO: in the SpellManager aswell, otherwise you will get a NullReferenceException
+            //Circle.Draw(Color.Red, SpellManager.W.Range, Player.Instance.Position);
+            Circle.Draw(Color.Red, SpellManager.E.Range, Player.Instance.Position);
+            Circle.Draw(Color.Red, SpellManager.R.Range, Player.Instance.Position);
+        }
+    }
+}
